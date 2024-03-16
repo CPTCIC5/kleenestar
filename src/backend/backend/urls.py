61998cbd115c3b8f9f311b0a94c839499753e802
Rashaad <rashaadzma@gmail.com>
2024-03-16 22:17:46 +0530

    Major refactor

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kleenestar project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/',include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
 ]
-urlpatterns += static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/backend/channels/models.py
+++ src/backend/channels/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.core.validators import validate_image_file_extension
 from users.models import Team
+
+
 # Create your models here.
 class Credential(models.Model):
-    api_code = models.CharField() #token api inputted via user
+    api_code = models.CharField(max_length=4000)  # token api inputted via user
 
     def __str__(self):
         return self.api_code
 
+
 class Channel(models.Model):
-    team = models.ForeignKey(Team,on_delete=models.CASCADE)
-    logo = models.ImageField(upload_to='Channel-Logo',validators=[validate_image_file_extension])
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    logo = models.ImageField(
+        upload_to="Channel-Logo", validators=[validate_image_file_extension]
+    )
     name = models.CharField(max_length=40)
     basic_info = models.CharField(max_length=150)
     desc = models.TextField()
-    credential = models.ForeignKey(Credential,on_delete=models.CASCADE)
-    
+    credential = models.ForeignKey(Credential, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
