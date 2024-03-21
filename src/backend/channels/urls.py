5a8aa617be2a8c3b5856297c9425432e01c2592c
ARYAN <aryanjainak@gmail.com>
2024-03-21 10:20:43 -0700

    fixed

--- src/backend/channels/urls.py
+++ src/backend/channels/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('prompt-feedback/',views.PromptFeedbackView.as_view(),name='feedback'),
-    path('channels/',views.ChannelView.as_view(),name='channels')
+    path('channels/',views.ChannelViewSet.as_view(),name='channels')
 ]
