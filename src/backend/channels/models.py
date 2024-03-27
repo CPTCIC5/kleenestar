5bd1db2998c688d80a4108001aa066f776ecc99b
Rashaad <rashaadzma@gmail.com>
2024-03-27 22:13:29 +0530

    add model

--- src/backend/channels/models.py
+++ src/backend/channels/models.py
@@ -44,7 +44,6 @@ class PromptFeedback(models.Model):
     def __str__(self):
         return str(self.user)
     
-"""
 class PromptInput(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     workspace = models.ForeignKey(WorkSpace, on_delete=models.CASCADE)
@@ -52,4 +51,3 @@ class PromptInput(models.Model):
     refactored_query = models.TextField(max_length=20_000)
     response_text = models.TextField(max_length=20_000)
     created = models.DateTimeField()
-"""
