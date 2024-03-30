512e63137733e593d6e66b44ce5f20a924d98c2c
Harsh Yadav <harsh12codes@gmail.com>
2024-03-31 01:20:43 +0530

    Finished PasswordRecovery , PasswordSaved, RecoveryEmail pages

--- src/frontend/src/pages/PasswordRecovery.tsx
+++ src/frontend/src/pages/PasswordRecovery.tsx
@@ -1,27 +1,137 @@
-import { FunctionComponent } from "react";
-import FrameComponent12 from "../components/FrameComponent12";
+import { Eye, EyeOff } from "lucide-react";
+import { FunctionComponent, useState } from "react";
+import PrimaryInputBox from "../components/PrimaryInputBox";
+import PrimaryButton from "../components/PrimaryButton";
 
 const PasswordRecovery: FunctionComponent = () => {
-  return (
-    <div className="w-full relative overflow-hidden flex flex-col items-center justify-start pt-[30px] px-5 pb-[0.4000000000001px] box-border gap-[91.69999999999982px] tracking-[normal] text-left text-6xl text-darkslateblue-100 font-syne mq450:gap-[23px_91.7px] mq750:gap-[46px_91.7px]">
-      <div className="w-[722px] h-[52.3px] flex flex-row items-start justify-center max-w-full">
-        <div className="self-stretch flex flex-row items-start justify-start gap-[17.800000000000182px]">
-          <img
-            className="h-[52.3px] w-[49.7px] relative shrink-0 [debug_commit:f6aba90]"
-            loading="lazy"
-            alt=""
-            src="/group-672.svg"
-          />
-          <div className="flex flex-col items-start justify-start pt-[11px] px-0 pb-0">
-            <h2 className="m-0 relative text-inherit font-bold font-inherit shrink-0 [debug_commit:f6aba90] mq450:text-xl">
-              Kleenestar
-            </h2>
-          </div>
+    const [passwordShow1, setPasswordShow1] = useState<boolean>(false);
+    const [passwordShow2, setPasswordShow2] = useState<boolean>(false);
+    const [password, setPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
+    const [passwordUnmatch, setPasswordUnmatch] = useState<boolean>(false);
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+        if (event.target.value !== confirmPassword) {
+            setPasswordUnmatch(true);
+        } else {
+            setPasswordUnmatch(false);
+        }
+    };
+
+    const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setConfirmPassword(event.target.value);
+        if (event.target.value !== password) {
+            setPasswordUnmatch(true);
+        } else {
+            setPasswordUnmatch(false);
+        }
+    };
+
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(password, confirmPassword);
+        {
+            /* add axios post request here */
+        }
+    };
+
+    return (
+        <div className="w-full h-screen flex items-center justify-center bg-background p-4">
+            <div className="max-w-[722px] max-h-[552.63px] w-full h-full flex flex-col items-center justify-center rounded-3xl p-4 relative ">
+                <div className="max-width flex items-center justify-center box-border max-w-full text-11xl font-syne">
+                    <div className="flex-1 flex flex-col items-center justify-center gap-[19px] max-w-full">
+                        <span className=" m-0 text-inherit font-bold font-inherit inline-block z-[1]">
+                            Password
+                        </span>
+                        <div className="w-full self-stretch text-base text-center font-montserrat z-[1] flex flex-col item-center">
+                            <span>Reset your password by creating a new one.</span>
+                        </div>
+                    </div>
+                </div>
+
+                <form
+                    method="post"
+                    onSubmit={handleFormSubmit}
+                    className={`max-w-[454px] w-full max-h-[434px] mt-[39px] flex flex-col items-center gap-[16px]`}
+                >
+                    <div className="w-full h-[72.33px] gap-[10px] flex flex-col justify-between">
+                        <span className="w-full h-[17px] font-montserrat font-[500] text-[14px] leading-[17.07px] text-primary">
+                            New Password*
+                        </span>
+                        <div className="relative w-full h-[45px] flex items-center ">
+                            <PrimaryInputBox
+                                type={passwordShow1 ? "text" : "password"}
+                                placeholder="Password"
+                                name="password"
+                                onChange={handlePasswordChange}
+                                className="focus:outline-primary-100 focus:outline"
+                                value={password}
+                                required
+                            />
+                            {/*PrimaryInputBox component for password*/}
+                            <div
+                                onClick={() => setPasswordShow1(!passwordShow1)}
+                                className="absolute bg-background text-primary flex items-center right-4 cursor-pointer"
+                            >
+                                {passwordShow1 ? (
+                                    <EyeOff className="bg-inherit" />
+                                ) : (
+                                    <Eye className="bg-inherit" />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className={`w-full  ${
+                            passwordUnmatch ? "h-[99.66px]" : "h-[73.66px]"
+                        } gap-[10px] flex flex-col justify-between`}
+                    >
+                        <span className="w-full h-[17px] font-montserrat font-[500] text-[14px] leading-[17.07px] text-primary">
+                            Confirm Password*
+                        </span>
+                        <div className="relative w-full h-[45px] flex items-center ">
+                            <PrimaryInputBox
+                                type={passwordShow2 ? "text" : "password"}
+                                placeholder="Confirm Password"
+                                name="confirm_password"
+                                onChange={handleConfirmPasswordChange}
+                                className="focus:outline-primary-100 focus:outline"
+                                value={confirmPassword}
+                                required
+                            />
+                            {/*PrimaryInputBox component for password*/}
+                            <div
+                                onClick={() => setPasswordShow2(!passwordShow2)}
+                                className="absolute bg-background text-primary flex items-center right-4 cursor-pointer"
+                            >
+                                {passwordShow2 ? (
+                                    <EyeOff className="bg-inherit" />
+                                ) : (
+                                    <Eye className="bg-inherit" />
+                                )}
+                            </div>
+                        </div>
+
+                        {passwordUnmatch && (
+                            <div className="w-full h-[16pxpx] flex items-center justify-start">
+                                <span className=" h-[16px] font-montserrat font-[300] text-[13px] leading-[15.85px] text-orangered-300">
+                                    {passwordUnmatch ? "Password doesn't match" : ""}
+                                </span>
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="h-[40px] max-w-[454px] w-full mt-[23px]">
+                        <PrimaryButton disabled={passwordUnmatch || !password || !confirmPassword}>
+                            Save password
+                        </PrimaryButton>
+                        {/* Use the PrimaryButton component */}
+                    </div>
+                </form>
+            </div>
         </div>
-      </div>
-      <FrameComponent12 />
-    </div>
-  );
+    );
 };
 
 export default PasswordRecovery;
