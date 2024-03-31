eb953e480b1691f95baa7db8c290ac1e4738babc
ARYAN <aryanjainak@gmail.com>
2024-04-01 01:22:46 +0530

    django-cors-headers

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -34,9 +34,11 @@ INSTALLED_APPS = [
     "dummy.apps.DummyConfig",
     "rest_framework",
     "django_rest_passwordreset",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -44,6 +46,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = "backend.urls"
--- src/backend/channels/get_campaigns.py
+++ src/backend/channels/get_campaigns.py
@@ -1,60 +1,8 @@
-import argparse
-import sys
-
 from google.ads.googleads.client import GoogleAdsClient
-from google.ads.googleads.errors import GoogleAdsException
+import os
 
-# [START get_campaigns]
-def main(client, customer_id):
-    ga_service = client.get_service("GoogleAdsService")
+os.environ["GOOGLE_ADS_CONFIGURATION_FILE_PATH"] = "path/to/google-ads.yaml"
+from google.ads.googleads.client import GoogleAdsClient
+client = GoogleAdsClient.load_from_storage()
 
-    query = """
-        SELECT
-          campaign.id,
-          campaign.name
-        FROM campaign
-        ORDER BY campaign.id"""
-
-    # Issues a search request using streaming.
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-
-    for batch in stream:
-        for row in batch.results:
-            print(
-                f"Campaign with ID {row.campaign.id} and name "
-                f'"{row.campaign.name}" was found.'
-            )
-            # [END get_campaigns]
-
-
-if __name__ == "__main__":
-    # GoogleAdsClient will read the google-ads.yaml configuration file in the
-    # home directory if none is specified.
-    googleads_client = GoogleAdsClient.load_from_storage(version="v16")
-
-    parser = argparse.ArgumentParser(
-        description="Lists all campaigns for specified customer."
-    )
-    # The following argument(s) should be provided to run the example.
-    parser.add_argument(
-        "-c",
-        "--customer_id",
-        type=str,
-        required=True,
-        help="The Google Ads customer ID.",
-    )
-    args = parser.parse_args()
-
-    try:
-        main(googleads_client, args.customer_id)
-    except GoogleAdsException as ex:
-        print(
-            f'Request with ID "{ex.request_id}" failed with status '
-            f'"{ex.error.code().name}" and includes the following errors:'
-        )
-        for error in ex.failure.errors:
-            print(f'\tError with message "{error.message}".')
-            if error.location:
-                for field_path_element in error.location.field_path_elements:
-                    print(f"\t\tOn field: {field_path_element.field_name}")
-        sys.exit(1)
+client = GoogleAdsClient.load_from_storage("path/to/google-ads.yaml")
