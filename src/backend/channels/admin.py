654efe1d180672d8414a03d43ff8ed3ef4abbc94
ARYAN <aryanjainak@gmail.com>
2024-04-05 11:58:24 +0530

    prompt-input

--- src/backend/channels/admin.py
+++ src/backend/channels/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Channel,PromptFeedback,APICredentials,Prompt
+from .models import Channel,PromptFeedback,APICredentials,Prompt,Convo
 # Register your models here.
 
 admin.site.register(Channel)
 admin.site.register(PromptFeedback)
 admin.site.register(APICredentials)
+admin.site.register(Convo)
 admin.site.register(Prompt)
