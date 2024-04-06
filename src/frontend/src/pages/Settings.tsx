b7ba6ce4e6b1d5428a0292b7b1bd15317a694fe0
Griffin Annshual <griffintbr@gmail.com>
2024-04-06 22:11:21 +0530

    feat(ui): added connect channel page ui, fixed drop down ui in setting page

--- src/frontend/src/pages/Settings.tsx
+++ src/frontend/src/pages/Settings.tsx
@@ -4,14 +4,26 @@ import ProfileSection from "../components/ProfileSection"
 import SlideSwitch from "../components/SlideSwitch"
 import { ChevronDown } from "lucide-react"
 import { FolderClosed } from "lucide-react"
-
+import { useNavigate } from "react-router-dom"
+import SelectOptions from "../components/SelectOptions"
 
 function Settings(): JSX.Element {
+	const reasonsToDeleteAccount = [
+		"Found a better platform/service",
+		"Concerns about privacy/security",
+		"No longer need the account/service",
+		"Dissatisfaction with the platform/service",
+		"Simplifying digital footprint",
+	]
+
+  const navigate = useNavigate()
   return (
 		<div className="w-screen bg-background pb-20">
 			<div className="pt-10 pl-8 mq750:pl-0 ">
 				<div className="text-primary-300 mq750:px-10 flex gap-8 mq750:justify-between items-center">
-					<div className="border px-[0.6rem] border-solid  border-gray-700 w-fit rounded-full py-[0.5rem] mq750:px-[0.4rem] mq750:py-0 ">
+					<div
+						onClick={() => navigate(-1)}
+						className="border px-[0.6rem] border-solid  border-gray-700 w-fit rounded-full py-[0.5rem] mq750:px-[0.4rem] mq750:py-0 ">
 						<ArrowLeft className="mq750:w-[0.6rem] h-fit w-8" />
 					</div>
 					<div className="text-primary-300 font-syne text-[1.6rem] font-bold mq750:text-[20px]">
@@ -38,7 +50,7 @@ function Settings(): JSX.Element {
 						<div className="text-primary-300 font-montserrat pt-2 pl-8 pb-4 my-4 ">
 							<div className=" font-bold pr-10 mq750:pr-4 mq750:text-[16px]  text-[1.6rem] flex justify-between my-4">
 								Google Authenticator
-								<SlideSwitch />
+								<SlideSwitch on={true} />
 							</div>
 							<p className="mq750:text-[14px] mq750:pr-2">
 								Use the Google Authenticator app to generate one time security
@@ -50,7 +62,7 @@ function Settings(): JSX.Element {
 						<div className="text-primary-300 font-montserrat pt-2 pl-8 pb-4">
 							<div className=" font-bold mq750:text-[16px] pr-10 mq750:pr-4 text-[1.6rem] flex justify-between my-4">
 								Two Factor Authentication
-								<SlideSwitch />
+								<SlideSwitch on={true} />
 							</div>
 							<p className="mq750:text-[14px] mq750:pr-2">
 								Require a second authentication method in addition to your
@@ -70,18 +82,17 @@ function Settings(): JSX.Element {
 						from our service, and purge all your information from our database.
 						This cannot be undone. 😱
 					</p>
-					<div className="flex justify-between items-center pr-4 pb-4 mq750:flex-col"	>
+					<div className="flex justify-between items-center pr-4 pb-4 mq750:flex-col">
 						<div className="w-[60%]  mq750:w-full text-primary-300 font-bold text-xl">
 							<div className=" mt-2 mq750:h-[45px] bg-background rounded-[2rem] p-4 flex items-center ">
-								<input
-									type="text"
-									placeholder="To confirm, tell us why you are leaving."
-									className="font-montserrat flex w-[90%]  text-xl mq750:text-[12px]"
+								<SelectOptions
+									options={reasonsToDeleteAccount}
+									InputText={"To confirm, tell us why you are leaving."}
 								/>
 								<ChevronDown className="text-primary-300" />
 							</div>
 						</div>
-						<div className="border-2 items-center flex whitespace-nowrap mq750:text-[15px] mq750:mt-4 mq750:py-[0.5rem] mq750:px-16 hover:text-white hover:bg-red-500 cursor-pointer w-fit px-10 rounded-[2rem] py-[1rem] border-solid border-red-500 text-red-500"	>
+						<div className="border-2 items-center flex whitespace-nowrap mq750:text-[15px] mq750:mt-4 mq750:py-[0.5rem] mq750:px-16 hover:text-white hover:bg-red-500 cursor-pointer w-fit px-10 rounded-[2rem] py-[1rem] border-solid border-red-500 text-red-500">
 							Delete Account
 						</div>
 					</div>
