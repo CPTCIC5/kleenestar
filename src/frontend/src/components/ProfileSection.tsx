2274d5458d2abb94572261cebf4342526fcb4868
Griffin Annshual <griffintbr@gmail.com>
2024-04-10 01:09:59 +0530

    Fixes: Applied css to input component, shifted contry data to countries.json

--- src/frontend/src/components/ProfileSection.tsx
+++ src/frontend/src/components/ProfileSection.tsx
@@ -2,208 +2,10 @@ import { PencilLine } from "lucide-react"
 import { ChevronDown } from "lucide-react"
 import {useRef} from 'react'
 import SelectOptions from "./SelectOptions";
-
+import {countries} from '../utils/countries.json'
 
 function ProfileSection(): JSX.Element {
-	const countries = [
-		"Afghanistan",
-		"Albania",
-		"Algeria",
-		"Andorra",
-		"Angola",
-		"Antigua and Barbuda",
-		"Argentina",
-		"Armenia",
-		"Australia",
-		"Austria",
-		"Azerbaijan",
-		"Bahamas",
-		"Bahrain",
-		"Bangladesh",
-		"Barbados",
-		"Belarus",
-		"Belgium",
-		"Belize",
-		"Benin",
-		"Bhutan",
-		"Bolivia",
-		"Bosnia and Herzegovina",
-		"Botswana",
-		"Brazil",
-		"Brunei",
-		"Bulgaria",
-		"Burkina Faso",
-		"Burundi",
-		"Cabo Verde",
-		"Cambodia",
-		"Cameroon",
-		"Canada",
-		"Central African Republic",
-		"Chad",
-		"Chile",
-		"China",
-		"Colombia",
-		"Comoros",
-		"Congo, Democratic Republic of the",
-		"Congo, Republic of the",
-		"Costa Rica",
-		"Croatia",
-		"Cuba",
-		"Cyprus",
-		"Czech Republic",
-		"Denmark",
-		"Djibouti",
-		"Dominica",
-		"Dominican Republic",
-		"East Timor (Timor-Leste)",
-		"Ecuador",
-		"Egypt",
-		"El Salvador",
-		"Equatorial Guinea",
-		"Eritrea",
-		"Estonia",
-		"Eswatini",
-		"Ethiopia",
-		"Fiji",
-		"Finland",
-		"France",
-		"Gabon",
-		"Gambia",
-		"Georgia",
-		"Germany",
-		"Ghana",
-		"Greece",
-		"Grenada",
-		"Guatemala",
-		"Guinea",
-		"Guinea-Bissau",
-		"Guyana",
-		"Haiti",
-		"Honduras",
-		"Hungary",
-		"Iceland",
-		"India",
-		"Indonesia",
-		"Iran",
-		"Iraq",
-		"Ireland",
-		"Israel",
-		"Italy",
-		"Ivory Coast",
-		"Jamaica",
-		"Japan",
-		"Jordan",
-		"Kazakhstan",
-		"Kenya",
-		"Kiribati",
-		"Korea, North",
-		"Korea, South",
-		"Kosovo",
-		"Kuwait",
-		"Kyrgyzstan",
-		"Laos",
-		"Latvia",
-		"Lebanon",
-		"Lesotho",
-		"Liberia",
-		"Libya",
-		"Liechtenstein",
-		"Lithuania",
-		"Luxembourg",
-		"Madagascar",
-		"Malawi",
-		"Malaysia",
-		"Maldives",
-		"Mali",
-		"Malta",
-		"Marshall Islands",
-		"Mauritania",
-		"Mauritius",
-		"Mexico",
-		"Micronesia",
-		"Moldova",
-		"Monaco",
-		"Mongolia",
-		"Montenegro",
-		"Morocco",
-		"Mozambique",
-		"Myanmar (Burma)",
-		"Namibia",
-		"Nauru",
-		"Nepal",
-		"Netherlands",
-		"New Zealand",
-		"Nicaragua",
-		"Niger",
-		"Nigeria",
-		"North Macedonia",
-		"Norway",
-		"Oman",
-		"Pakistan",
-		"Palau",
-		"Palestine",
-		"Panama",
-		"Papua New Guinea",
-		"Paraguay",
-		"Peru",
-		"Philippines",
-		"Poland",
-		"Portugal",
-		"Qatar",
-		"Romania",
-		"Russia",
-		"Rwanda",
-		"Saint Kitts and Nevis",
-		"Saint Lucia",
-		"Saint Vincent and the Grenadines",
-		"Samoa",
-		"San Marino",
-		"Sao Tome and Principe",
-		"Saudi Arabia",
-		"Senegal",
-		"Serbia",
-		"Seychelles",
-		"Sierra Leone",
-		"Singapore",
-		"Slovakia",
-		"Slovenia",
-		"Solomon Islands",
-		"Somalia",
-		"South Africa",
-		"South Sudan",
-		"Spain",
-		"Sri Lanka",
-		"Sudan",
-		"Suriname",
-		"Sweden",
-		"Switzerland",
-		"Syria",
-		"Taiwan",
-		"Tajikistan",
-		"Tanzania",
-		"Thailand",
-		"Togo",
-		"Tonga",
-		"Trinidad and Tobago",
-		"Tunisia",
-		"Turkey",
-		"Turkmenistan",
-		"Tuvalu",
-		"Uganda",
-		"Ukraine",
-		"United Arab Emirates",
-		"United Kingdom",
-		"United States",
-		"Uruguay",
-		"Uzbekistan",
-		"Vanuatu",
-		"Vatican City",
-		"Venezuela",
-		"Vietnam",
-		"Yemen",
-		"Zambia",
-		"Zimbabwe",
-	]
+
 	const imageRef = useRef<HTMLInputElement>(null);
 	const handleAvatarClick = () => {
 		if (imageRef.current) {
@@ -245,7 +47,7 @@ function ProfileSection(): JSX.Element {
 								<input
 									type="text"
 									placeholder="First name"
-									className="font-montserrat w-full text-xl mq750:text-[15px]"
+									className="font-montserrat autofill:bg-transparent outline-none bg-transparent w-full text-xl mq750:text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
@@ -270,9 +72,9 @@ function ProfileSection(): JSX.Element {
 									pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 									type="text"
 									placeholder="Email address"
-									className="font-montserrat w-full text-xl mq750:text-[15px]"
+									className="autofill:bg-transparent outline-none bg-transparent font-montserrat w-full text-xl mq750:text-[15px]"
 								/>
-								<PencilLine className="text-primary-300" />
+								<PencilLine className="text-primary-300 bg-transparent" />
 							</div>
 						</div>
 						<div className="w-full text-primary-300 font-bold text-xl mq750:text-[14px]">
@@ -281,7 +83,7 @@ function ProfileSection(): JSX.Element {
 								<input
 									type="text"
 									placeholder="Workspace name"
-									className="w-full font-montserrat text-xl mq750:text-[15px]"
+									className="autofill:bg-transparent outline-none bg-transparent w-full font-montserrat text-xl mq750:text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
@@ -304,7 +106,7 @@ function ProfileSection(): JSX.Element {
 								<input
 									type="text"
 									placeholder="Phone number"
-									className="w-full font-montserrat text-xl mq750:text-[15px]"
+									className="autofill:bg-transparent outline-none bg-transparent w-full font-montserrat text-xl mq750:text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
