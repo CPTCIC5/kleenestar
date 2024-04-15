44ae645c578ac6e83b80a5940a326717e7fab435
Griffin Annshual <griffintbr@gmail.com>
2024-04-15 23:30:54 +0530

    Added form Validations to settings page

--- src/frontend/src/components/ProfileSection.tsx
+++ src/frontend/src/components/ProfileSection.tsx
@@ -1,17 +1,94 @@
 import { PencilLine } from "lucide-react"
 import { ChevronDown } from "lucide-react"
-import {useRef} from 'react'
-import SelectOptions from "./SelectOptions";
-import {countries} from '../utils/countries.json'
+import { useRef } from "react"
+import SelectOptions from "./SelectOptions"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import parsePhoneNumber, { CountryCode } from "libphonenumber-js"
+import { getNames, registerLocale } from "i18n-iso-countries"
+import english from "i18n-iso-countries/langs/en.json"
+
+registerLocale(english)
+
+const countryList = Object.entries(getNames("en")).map(([value, name]) => ({
+	name,
+	value,
+}))
+type FormFields = z.infer<typeof schema>
+
+const schema = z.object({
+	first_name: z
+		.string()
+		.min(5, "The first name should be at least 5 characters long")
+		.max(100, "The first name should not exceed 100 characters")
+		.regex(/^[a-zA-Z '-]+$/, "Name can only include letters and hypen(-)"),
+	last_name: z
+		.string()
+		.min(5, "The last name should be at least 5 characters long")
+		.max(100, "The last name should not exceed 100 characters")
+		.regex(/^[a-zA-Z '-]+$/, "Name can only include letters and hypen(-)"),
+	email: z.string().email({ message: "Invalid email address" }),
+	workspace: z
+		.string()
+		.min(5, "Workspace name should have atleast 5 characters")
+		.max(100, "Workspace length should not exceed 100 characters ")
+		.regex(
+			/^[a-zA-Z0-9 _-]+$/,
+			"Workspace name can include letters, underscores, hypens(-) and numbers only"
+		),
+	country: z.string().min(1, { message: "Country is required" }),
+	number: z.string().min(10, {
+		message: "Invalid Phone Number"
+	}),
+})
 
 function ProfileSection(): JSX.Element {
-
-	const imageRef = useRef<HTMLInputElement>(null);
+	const imageRef = useRef<HTMLInputElement>(null)
 	const handleAvatarClick = () => {
 		if (imageRef.current) {
-			imageRef.current.click();
+			imageRef.current.click()
 		}
-	};
+	}
+
+	const form = useForm<FormFields>({
+		defaultValues: {
+			first_name: "",
+			last_name: "",
+			email: "",
+			workspace: "",
+			country: "",
+			number: "",
+		},
+		resolver: zodResolver(schema),
+	})
+	const { register, handleSubmit, formState, setError } = form
+	const { errors,isValid } = formState
+
+	const onSubmit: SubmitHandler<FormFields> = (data) => {
+		const validPhoneNumber = parsePhoneNumber(
+			data.number,
+			data.country as CountryCode
+		)
+		if (
+			!validPhoneNumber ||
+			!validPhoneNumber.isValid() ||
+			!(validPhoneNumber.country === data.country)
+		) {
+			setError("number", {
+				message: "Invalid Phone Number",
+			})
+			return
+		}
+		if (!data.country) {
+			setError("number", {
+				message: "Country Not Selected!",
+			})
+			return
+		}
+		console.log(data)
+	}
+
 	return (
 		<div className="w-[50%] bg-white mq1000:w-[95%] mq1000:mx-auto rounded-[2rem] ">
 			<div className="text-primary-300  font-montserrat pt-[28px] pl-8 pb-4 mq1000:pl-4 border-solid border-b-2 border-[#BEB9B1] ">
@@ -40,79 +117,130 @@ function ProfileSection(): JSX.Element {
 			</div>
 			<div className="p-2 pt-4  w-full font-montserrat text-lg ">
 				<div className="w-[95%] mx-auto">
-					<div className="flex items-center justify-center mq1000:flex-col gap-4">
+					<div className="flex  justify-center mq1000:flex-col gap-4">
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							First name
 							<div className="w-full mt-2 bg-background rounded-[2rem] p-4 flex items-center h-[45px] ">
 								<input
+									{...register("first_name", {
+										required: "First Name is required",
+									})}
 									type="text"
 									placeholder="First name"
-									className="font-montserrat autofill:bg-transparent outline-none bg-transparent w-full text-[15px]"
+									className="font-montserrat autofill:bg-transparent bg-clip-text outline-none bg-transparent w-full text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
+							{errors.first_name && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.first_name.message}
+								</div>
+							)}
 						</div>
+
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							Last name
 							<div className="w-full mt-2 bg-background h-[45px] rounded-[2rem] p-4 flex items-center ">
 								<input
+									{...register("last_name", {
+										required: "Last Name is required",
+									})}
 									type="text"
 									placeholder="Last name"
-									className="autofill:bg-transparent outline-none bg-transparent w-full font-montserrat  text-[15px]"
+									className="autofill:bg-transparent bg-clip-text outline-none bg-transparent w-full font-montserrat  text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
+							{errors.last_name && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.last_name.message}
+								</div>
+							)}
 						</div>
 					</div>
-					<div className="flex items-center justify-center gap-4 pt-2 mq1000:flex-col">
+					<div className="flex  justify-center gap-4 pt-2 mq1000:flex-col">
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							Email address
 							<div className="w-full h-[45px] mt-2 bg-[#D1D3DB] rounded-[2rem] p-4 flex items-center ">
 								<input
-									pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+									{...register("email", {
+										required: "Email is required",
+									})}
 									type="text"
 									placeholder="Email address"
-									className="autofill:bg-transparent outline-none bg-transparent font-montserrat w-full text-[15px]"
+									className="autofill:bg-transparent outline-none bg-transparent font-montserrat w-full text-[15px] bg-clip-text"
 								/>
 								<PencilLine className="text-primary-300 bg-transparent" />
 							</div>
+							{errors.email && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.email.message}
+								</div>
+							)}
 						</div>
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							Workspace name
 							<div className="w-full mt-2 h-[45px] bg-background rounded-[2rem] p-4 flex items-center ">
 								<input
+									{...register("workspace", {
+										required: "Workspace name is required",
+									})}
 									type="text"
 									placeholder="Workspace name"
-									className="autofill:bg-transparent outline-none bg-transparent w-full font-montserrat text-[15px]"
+									className="autofill:bg-transparent bg-clip-text outline-none bg-transparent w-full font-montserrat text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
+							{errors.workspace && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.workspace.message}
+								</div>
+							)}
 						</div>
 					</div>
-					<div className="flex items-center justify-center gap-4 pt-2 mq1000:flex-col">
+					<div className="flex  justify-center gap-4 pt-2 mq1000:flex-col">
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							Country
 							<div className="w-full px-[19.34px]  mt-2 bg-background  rounded-[2rem]  flex items-center ">
 								<SelectOptions
-									options={countries}
+									{...register("country", {
+										required: "Country is required",
+									})}
+									options={countryList}
 									InputText={"Country"}
 								/>
 								<ChevronDown className="text-primary-300 w-fit" />
 							</div>
+							{errors.country && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.country.message}
+								</div>
+							)}
 						</div>
 						<div className="w-full text-primary-300 font-bold text-[14px]">
 							Phone number
 							<div className="w-full h-[45px] mt-2 bg-background rounded-[2rem] p-4 flex items-center ">
 								<input
+									{...register("number", {
+										required: "Phone Number is required",
+									})}
 									type="text"
 									placeholder="Phone number"
-									className="autofill:bg-transparent outline-none bg-transparent w-full font-montserrat text-[15px]"
+									className="autofill:bg-transparent bg-clip-text outline-none bg-transparent w-full font-montserrat text-[15px]"
 								/>
 								<PencilLine className="text-primary-300" />
 							</div>
+							{errors.number && (
+								<div className="text-red-500 font-[300] text-sm  relative text-right  p-2">
+									{errors.number.message}
+								</div>
+							)}
 						</div>
 					</div>
-					<div className="w-full font-bold rounded-[2rem] my-8 cursor-pointer text-white text-[15px]  mq1000:py-[0.7rem] bg-primary-300 text-center py-[10px]">
+					<div
+						aria-disabled={!isValid}
+						onClick={handleSubmit(onSubmit)}
+						className="w-full font-bold rounded-[2rem] my-8 cursor-pointer text-white text-[15px]  mq1000:py-[0.7rem] bg-primary-300 text-center py-[10px]">
 						Save changes
 					</div>
 				</div>
--- src/frontend/src/components/SelectOptions.tsx
+++ src/frontend/src/components/SelectOptions.tsx
@@ -1,98 +1,105 @@
-import * as React from "react";
-import { Theme, useTheme } from "@mui/material/styles";
-import Input from "@mui/material/Input";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
+import * as React from "react"
+import { Theme, useTheme } from "@mui/material/styles"
+import Input from "@mui/material/Input"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import Select, { SelectChangeEvent } from "@mui/material/Select"
+import InputLabel from "@mui/material/InputLabel"
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
-};
-
-function getStyles(
-  name: string,
-  personName: string[],
-  theme: Theme
-) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium
-  };
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+		},
+	},
 }
 
-function SelectOptions({ options, InputText }: { options: Array<string>, InputText: string }) {
-  const names = options;
-const isSmallScreen = window.innerWidth < 1000
-const theme = useTheme();
-const [personName, setPersonName] = React.useState<string>("");
-
-const handleChange = (
-	event: SelectChangeEvent<typeof personName>
-) => {
-	const {
-		target: { value }
-	} = event;
-	setPersonName(value);
-};
-
-return (
-	<div className="w-full">
-		<FormControl className="w-full">
-			{!personName && (
-				<InputLabel
-					shrink={false}
-					className="text-gray-500 focus:text-gray-500   border-transparent font-montserrat  text-[15px]">
-					{InputText}
-				</InputLabel>
-			)}
-			<Select
-				labelId="demo-multiple-name-label"
-				id="demo-multiple-name"
-				value={personName}
-				onChange={handleChange}
-				input={
-					personName ? (
-						<Input
-							disableUnderline
-							className="w-full  bg-transparent max-h-[47px] h-full  border-transparent font-montserrat text-[15px]"
-						/>
-					) : (
-						<Input
-							disableUnderline
-							className="w-full px-[10px]  bg-transparent  max-h-[47px] h-full  border-transparent font-montserrat text-[15px]"
-						/>
-					)
-				}
-				IconComponent={() => null} // This line hides the dropdown arrow
-				MenuProps={MenuProps}>
-				{names.map((name) => (
-					<MenuItem
-						key={name}
-						value={name}
-						style={{
-							...getStyles(name, [personName], theme),
-							fontFamily: "Montserrat",
-							fontWeight: 400,
-							fontSize: isSmallScreen ? "11px" : "14px",
-							lineHeight: isSmallScreen ? "15px" : "17.07px",
-						}}>
-						{name}
-					</MenuItem>
-				))}
-			</Select>
-		</FormControl>
-	</div>
-)
+function getStyles(name: string, personName: string[], theme: Theme) {
+	return {
+		fontWeight:
+			personName.indexOf(name) === -1
+				? theme.typography.fontWeightRegular
+				: theme.typography.fontWeightMedium,
+	}
 }
 
-export default SelectOptions;
+type ops = {
+	name: string
+	value: string
+}
+
+function SelectOptions({
+	options,
+	InputText,
+	...props
+}: {
+	options: Array<ops>
+	InputText: string
+}) {
+	const isSmallScreen = window.innerWidth < 1000
+	const theme = useTheme()
+	const [personName, setPersonName] = React.useState<string>("")
+
+	const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+		const {
+			target: { value },
+		} = event
+		setPersonName(value)
+	}
+
+	return (
+		<div className="w-full">
+			<FormControl className="w-full">
+				{!personName && (
+					<InputLabel
+						shrink={false}
+						className="text-gray-500 focus:text-gray-500   border-transparent font-montserrat  text-[15px]">
+						{InputText}
+					</InputLabel>
+				)}
+				<Select
+					labelId="demo-multiple-name-label"
+					id="demo-multiple-name"
+					value={personName}
+					onChange={handleChange}
+					input={
+						personName ? (
+							<Input
+								{...props}
+								disableUnderline
+								className="w-full  bg-transparent min-h-[47px] h-full  border-transparent font-montserrat text-[15px]"
+							/>
+						) : (
+							<Input
+								{...props}
+								disableUnderline
+								className="w-full px-[10px]  bg-transparent  max-h-[47px] h-full  border-transparent font-montserrat text-[15px]"
+							/>
+						)
+					}
+					IconComponent={() => null} // This line hides the dropdown arrow
+					MenuProps={MenuProps}>
+					{options.map((option) => (
+						<MenuItem
+							key={option.name}
+							value={option.value}
+							style={{
+								...getStyles(option.name, [personName], theme),
+								fontFamily: "Montserrat",
+								fontWeight: 400,
+								fontSize: isSmallScreen ? "11px" : "14px",
+								lineHeight: isSmallScreen ? "15px" : "17.07px",
+							}}>
+							{option.name}
+						</MenuItem>
+					))}
+				</Select>
+			</FormControl>
+		</div>
+	)
+}
+
+export default SelectOptions
