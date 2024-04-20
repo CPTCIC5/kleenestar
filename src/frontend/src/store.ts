83da70607334dc9336a628b41615b6e420c2098c
Harsh Yadav <harsh12codes@gmail.com>
2024-04-20 23:14:44 +0530

    Added store.ts (zustand)

--- src/frontend/src/store.ts
+++ src/frontend/src/store.ts
@@ -1,6 +1,42 @@
-import {create} from 'zustand'
+import { create } from "zustand";
 
+import { devtools, persist } from "zustand/middleware";
 
-type ChatStore = {
-    chat: [],
+interface Convo {
+    id: number;
+    assitant_id: string;
+    title: string;
+    created_at: string;
 }
+
+interface InputPrompt {
+    id: number;
+    convo_id: number;
+    author: string;
+    text_query: string;
+    file_query: string;
+    response_text: string;
+    response_image: string;
+    created_at: string;
+}
+
+interface ChatStoreState {
+    convos: Convo[];
+    inputPrompts: InputPrompt[];
+}
+
+const chatStore = create<ChatStoreState>((set) => ({
+    convos: [],
+    inputPrompts: [],
+    
+}));
+
+const useChatStore = create(
+    devtools(
+        persist(chatStore, {
+            name: "chat-store",
+        }),
+    ),
+);
+
+export default useChatStore;
