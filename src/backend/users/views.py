ada9b9dfba99572be13325bc822bbd3940ab1bff
ARYAN <aryanjainak@gmail.com>
2024-04-23 02:09:14 +0530

    PromptViewSet Get,delete

--- src/backend/users/views.py
+++ src/backend/users/views.py
@@ -88,6 +88,11 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_200_OK)
 
 
          
