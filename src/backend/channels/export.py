a68b36ca4ab34b69f9da26bd73b15b404621bce5
ARYAN <aryanjainak@gmail.com>
2024-06-02 00:18:30 +0530

    half=fix

--- src/backend/channels/export.py
+++ src/backend/channels/export.py
@@ -10,7 +10,10 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from oauth import views
+from channels.models import Channel
+from django.shortcuts import get_object_or_404
 
+"""
 
 @api_view(['GET'])
 @permission_classes([AllowAny])  # Allow access to anyone
@@ -36,27 +39,31 @@ def merge_json_files(request):
         merged_data.append(file_data)
 
     return Response(merged_data)
-
 """
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])  # Allow access logged in user
 def merge_json_files(request):
+    user= request.user
+    workspace= user.workspace_set.all()[0]
+    google_cred= get_object_or_404(Channel, workspace=workspace,channel_type=1).credentials
+    meta_cred= get_object_or_404(Channel, workspace=workspace,channel_type=2).credentials
+    twitter_cred= get_object_or_404(Channel, workspace=workspace,channel_type=3).credentials
+    linkedin_cred= get_object_or_404(Channel, workspace=workspace,channel_type=4).credentials
+    tiktok_cred= get_object_or_404(Channel, workspace=workspace,channel_type=5).credentials
+
 
     merged_data = []
     
     #Get data from each marketing channel function
-    google_data = views.get_google_marketing_data(key1, key3) #pass key1 and key3 as params
-    facebook_data = views.get_facebook_marketing_data(key1, key2) #pass key1 and key2 as params
-    twitter_data = views.get_twitter_marketing_data(key1,key2,key3,key4)
-    linkedin_data = views.get_linkedin_marketing_data(key1)
+    google_data = views.get_google_marketing_data(google_cred.key_1, google_cred.key_3) #pass key1 and key3 as params
+    facebook_data = views.get_facebook_marketing_data(meta_cred.key_1, meta_cred.key_2) #pass key1 and key2 as params
+    twitter_data = views.get_twitter_marketing_data(twitter_cred.key_1, twitter_cred.key_2, twitter_cred.key_3, twitter_cred.key_4)
+    linkedin_data = views.get_linkedin_marketing_data(linkedin_cred.key_1)
     tiktok_data = views.get_tiktok_marketing_data()
 
-    # Append each data to the merged_data list
-    merged_data.append(google_data)
-    merged_data.append(facebook_data)
-    merged_data.append(twitter_data)
-    merged_data.append(linkedin_data)
-    merged_data.append(tiktok_data)
+    merged_data.extend([google_data, facebook_data, twitter_data, linkedin_data, tiktok_data])
+
 
     return Response(merged_data)
-"""
