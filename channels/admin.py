407328332634d11918a007a5ee25c9e8be6ef541
ARYAN <aryanjainak@gmail.com>
2024-07-05 02:43:39 +0530

    parallel done

--- channels/admin.py
+++ channels/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Channel,PromptFeedback,APICredentials,Prompt,Convo,BlockNote,KnowledgeBase,Note
+from .models import Channel,PromptFeedback,APICredentials,Prompt,Convo,BlockNote,KnowledgeBase,Note,KnowledgeSource
 # Register your models here.
 
 admin.site.register(Channel)
@@ -11,4 +11,5 @@ admin.site.register(Prompt)
 admin.site.register(Note)
 admin.site.register(BlockNote)
 
+admin.site.register(KnowledgeSource)
 admin.site.register(KnowledgeBase)
--- channels/views.py
+++ channels/views.py
@@ -220,40 +220,33 @@ class BlockNoteViewSet(viewsets.ModelViewSet):
 class KnowledgeBaseView(viewsets.ModelViewSet):
     queryset = models.KnowledgeBase.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
-    serializer_class = serializers.KnowlodgeBaseSerializer
+    serializer_class = serializers.KnowledgeBaseSerializer
     filterset_fields = ('subspace',)
 
     def get_queryset(self):
         user = self.request.user
-        return models.KnowledgeBase.objects.filter(subspace__workspace=user.workspace_set.all()[0])
-    
+        return models.KnowledgeBase.objects.filter(workspace__user=user)
+
     def create(self, request, *args, **kwargs):
-        serializer = serializers.CreateKnowledgeBaseSerializer(
-            data=request.data
-        )
+        serializer = serializers.CreateKnowledgeBaseSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(
-            user=request.user,
-        )
+        workspace = request.user.workspace_set.first()
+        serializer.save(workspace=workspace)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = serializers.CreateKnowledgeBaseSerializer(
-            instance=instance,
-            data=request.data,
-            partial=partial
-        )
+        serializer = serializers.CreateKnowledgeBaseSerializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
-        return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class SubspaceViewSet(viewsets.ModelViewSet):
